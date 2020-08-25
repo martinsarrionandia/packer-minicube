7d752559502a01165b88813cc7ebfe83c59b131c
Martin Sarrionandia <martin@martin.tazer>
2020-08-25 23:11:15 +0100

    Sort out memory and cpus stuff

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-DRIVER="none"	#none or docker
-ADMIN_USER="kubeadmin"
-
 # Add kubeadmin user
 
 useradd $ADMIN_USER
@@ -31,16 +28,23 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s http
 chmod 755 kubectl
 mv kubectl /usr/local/bin
 
-# Set driver
+# Set config options
+
+FREEKB=$(cat /proc/meminfo |grep MemFree|awk '{print $2}')
+FREEMB=$((FREEKB/1024))
+BREATHSPACEMB=128
+KUBEMEM=$((FREEMB-BREATHSPACEMB))
 
 /usr/local/bin/minikube config set driver $DRIVER
+/usr/local/bin/minikube config set cpu $CPUS
+/usr/local/bin/minikube config set memory $KUBEMEM
 
 # Start minikube
 
-if [ $DRIVER="docker" ]
+if [ "$DRIVER" == "docker" ]
 then
   sudo -u $ADMIN_USER /usr/local/bin/minikube start
-elif [ $DRIVER="none"]
+elif [ "$DRIVER" == "none"]
 then
   /usr/local/bin/minikube start
 else
